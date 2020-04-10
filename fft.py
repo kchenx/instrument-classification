598476fc5c3595f20151fb957b20426815ee1765
Vinay Kanumuri <vinkanumuri@gmail.com>
2020-04-10 01:42:31 -0400

    averages
    altered data, sounds, images folder and commented additions to code.

--- fft.py
+++ fft.py
@@ -2,24 +2,28 @@ from matplotlib import pyplot as plt
 import numpy as np
 from scipy.fft import fft, fftfreq
 from scipy.io import wavfile as wav
+from scipy import optimize
 import os
 
 # Plot and save the Fourier transform of wav file at `path` to `dst`
 def compute_fft(path, imgdst, datadst = None):
+    # data processing and fft calculations
     rate, data = wav.read(path)
     try:
-        data = data[:,0]
+        data = data[:, 0]
     except IndexError:
         pass
     samples = data.shape[0]
     datafft = fft(data)
     fftabs = abs(datafft)
+    # TO DO: normalization of fft values
+    # TO DO: scaling + shifting of freq values
     freqs = fftfreq(samples, 1/rate)
-
     np.savetxt(datadst,
                np.c_[freqs[:int(freqs.size/2):1000], fftabs[:int(freqs.size/2):1000]],
                delimiter=",")
 
+    # creating image
     plt.figure()
     plt.xlim([10, rate/2])
     plt.xscale("log")
@@ -29,7 +33,16 @@ def compute_fft(path, imgdst, datadst = None):
     plt.plot(freqs[:int(freqs.size/2)], fftabs[:int(freqs.size/2)])
     plt.savefig(imgdst)
 
+    # instrument average calculations
+    fftabs = fftabs[:int(freqs.size / 2)]
+    freqs = freqs[:int(freqs.size / 2)]
+    lfreq = 20  # freq bounds
+    ufreq = 20000
+    ind = np.where(np.logical_and(freqs >= lfreq, freqs <= ufreq))
+    return freqs[ind], fftabs[ind]
+
 def compute_all_ffts():
+    piano = []   # initializing instrument average storage TO DO: implement for other instruments
     for root, dirs, files in os.walk("sounds"):
         for file in files:
             path = os.path.join(root, file)
@@ -38,4 +51,17 @@ def compute_all_ffts():
                 body = os.path.sep.join(pre.split(os.path.sep)[1:])
                 imgdst = os.path.join("images", body + ".png")
                 datadst = os.path.join("fft-data", body + ".csv")
-                compute_fft(path, imgdst, datadst)
+                freq, fft = compute_fft(path, imgdst, datadst)
+
+                # instrument average calculations
+                if 'piano' in path:
+                    # TO DO: convert freq, fft (discrete data) to smooth function and append to piano array
+
+                # elif 'xylophone' in path:
+    # avgpiano = np.average(piano, axis=0)
+    pass
+
+
+
+
+
